05fcb1f9193de93981fc271cac7ba55c2cd08f11
hsusanoo <35850056+hsusanoo@users.noreply.github.com>
2021-06-13 14:23:22 +0100

    Initial commit

---
+++ app/src/main/java/com/hai/yumy/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.hai.yumy
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.hai.yumy.ui.theme.YumyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            YumyTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    YumyTheme {
+        Greeting("Android")
+    }
+}
